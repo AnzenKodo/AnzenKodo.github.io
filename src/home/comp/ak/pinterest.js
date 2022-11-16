a09d01bcca572b784971ea11ff3b0e83ccf9a5f2
Tesura <AnzenKodo@altmails.com>
2022-11-16 10:16:45 +0530

    Update pinterest.js

--- src/home/comp/ak/pinterest.js
+++ src/home/comp/ak/pinterest.js
@@ -8,8 +8,7 @@ const html = await fetch(url).then((res) => res.text());
 const doc = new DOMParser().parseFromString(html, "text/html");
 
 async function getImg(query, imgOld) {
-  const imgCSS = doc.querySelector(query).getAttribute("style");
-  const imgUrl = imgCSS.match(/https:\/\/[\w./]*/)[0];
+  const imgUrl = doc.querySelector(query).getAttribute("src");
   const imgOg = imgUrl.replace(
     "https://i.pinimg.com/200x",
     "https://i.pinimg.com/originals",
@@ -26,14 +25,14 @@ async function getImg(query, imgOld) {
 
 export async function getBanner(imgOld) {
   const query =
-    "#mweb-unauth-container > div > div > div:nth-child(5) > div > div.F6l.k1A.zI7.iyn.Hsu > div > div:nth-child(2) > div > div > div.Pj7.sLG.XiG.ho-.m1e > div > div:nth-child(1) > div > div.XiG.zI7.iyn.Hsu > div";
+    "#mweb-unauth-container > div > div > div:nth-child(5) > div > div.F6l.k1A.zI7.iyn.Hsu > div > div:nth-child(2) > div > div > div.Pj7.sLG.XiG.ho-.m1e > div > div:nth-child(1) > div > div.XiG.zI7.iyn.Hsu > img";
 
   return await getImg(query, imgOld);
 }
 
 export async function getWallpaper(imgOld) {
   const query =
-    "#mweb-unauth-container > div > div > div:nth-child(5) > div > div.F6l.k1A.zI7.iyn.Hsu > div > div:nth-child(1) > div > div > div.Pj7.sLG.XiG.ho-.m1e > div > div:nth-child(1) > div > div.XiG.zI7.iyn.Hsu > div";
+    "#mweb-unauth-container > div > div > div:nth-child(5) > div > div.F6l.k1A.zI7.iyn.Hsu > div > div:nth-child(1) > div > div > div.Pj7.sLG.XiG.ho-.m1e > div > div:nth-child(1) > div > div.XiG.zI7.iyn.Hsu > img";
 
   return await getImg(query, imgOld);
 }
