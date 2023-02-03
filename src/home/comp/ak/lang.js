13e5889675cbc9e94cff1f1f4dfc1998a94c1565
Tesura <AnzenKodo@altmails.com>
2023-02-04 03:27:16 +0530

    Changed lang data get point

--- src/home/comp/ak/lang.js
+++ src/home/comp/ak/lang.js
@@ -1,7 +1,12 @@
-await fetch("https://metrics.lecoq.io/insights/AnzenKodo");
+import {
+  DOMParser,
+} from "https://deno.land/x/deno_dom@v0.1.32-alpha/deno-dom-wasm.ts";
 
-export const languages = await fetch(
-  "https://metrics.lecoq.io/insights/query/AnzenKodo/languages",
-)
-  .then((res) => res.json())
-  .then((res) => Object.keys(res.stats));
+const url =
+  "https://ionicabizau.github.io/github-profile-languages/api.html?AnzenKodo";
+const html = await fetch(url).then((res) => res.text());
+
+const doc = new DOMParser().parseFromString(html, "text/html");
+export const languages = Array.from(
+  doc.querySelectorAll("#pieChart > svg > g.legend > g"),
+).map((v) => v.textContent);
