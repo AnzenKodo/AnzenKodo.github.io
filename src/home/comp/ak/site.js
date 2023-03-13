c75fe9fd798bf735bdc1386fd7e7b10f8c70d945
Tesura <AnzenKodo@altmails.com>
2023-03-13 21:21:36 +0530

    Fix file path in ak.js

--- src/home/comp/ak/site.js
+++ src/home/comp/ak/site.js
@@ -2,10 +2,11 @@ import { parse } from "https://deno.land/std@0.179.0/path/mod.ts";
 
 export default function site(start_url, ignore) {
   const obj = {};
-  const names = [...Deno.readDirSync("src/home/pages/")]
-    .map((meta) => parse(meta.name).name)
-    .filter((name) => ignore.indexOf(name) === -1);
 
-  names.map((name) => obj[name] = `${start_url}${name}`);
+  [...Deno.readDirSync("./home/pages/")]
+    .map((meta) => parse(meta.name).name)
+    .filter((name) => ignore.indexOf(name) === -1)
+    .map((name) => obj[name] = `${start_url}${name}`);
+
   return obj;
 }
