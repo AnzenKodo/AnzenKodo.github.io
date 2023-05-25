09b98f90f9d4d5dfb2aa08906fb24e784ca7d72c
Tesura <AnzenKodo@altmails.com>
2023-05-25 14:18:59 +0530

    Changed blogroll yaml path

--- src/home/api/blogroll.js
+++ src/home/api/blogroll.js
@@ -1,7 +1,7 @@
 import { writeInOutput } from "../comp/utils.js";
 import { parse } from "npm:yaml";
 
-const yaml = Deno.readTextFileSync("notes/Feed.yaml");
+const yaml = Deno.readTextFileSync("data/feed.yaml");
 const json = JSON.stringify(parse(yaml), null, 2);
 
 writeInOutput("api/blogroll.json", json);
