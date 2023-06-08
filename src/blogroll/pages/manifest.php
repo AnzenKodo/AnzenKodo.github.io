dccae63d92e741d1d6815dd7fa0e3bd07890f4de
Tesura <AnzenKodo@altmails.com>
2023-06-08 08:38:28 +0530

    Fixed blogroll json

--- src/blogroll/pages/manifest.php
+++ src/blogroll/pages/manifest.php
@@ -1,21 +1,18 @@
 <?php
-// Decode the JSON file
-$data = (object)json_decode($json, true);
-
 $data = (object)array(
   "name" => "AK#Blogroll",
-  "author" => $data->name,
+  "author" => $json->name,
   "description" => "List of some awesome websites that you should subscribe.",
-	"email" => $data->email,
-  "home"=> $data->start_url,
-  "start_url" => "{$data->start_url}blogroll",
+	"email" => $json->email,
+  "home"=> $json->start_url,
+  "start_url" => "{$json->start_url}blogroll",
   "background_color" => "#ffffff",
   "foreground_color" => "#000000",
   "theme_color" => "#0583f2",
   "display" =>"standalone",
   "orientation" => "portrait",
-  "twitter" => $data->username,
-	"portfolio" => $data->start_url,
+  "twitter" => $json->username,
+	"portfolio" => $json->start_url,
 	"icons" => [
 		  "src" => "favicon.png",
       "type" => "image/png",
