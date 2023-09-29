34681ed2d64671c63a0c960f962a5417bae8e6b4
AnzenKodo <AnzenKodo@altmails.com>
2023-09-29 23:28:51 +0530

    Make run.sh file executeable

--- src/run.sh
+++ src/run.sh
@@ -40,3 +40,4 @@ echo "This is a Brave Rewards publisher verification file.
 Domain: anzenkodo.github.io
 Token: 4165d0e625cb72d07a870bbb7c17ef9583e535ce6ecd7a47284d965f87f2bc17
 " > $OUTPUT.well-know/brave-rewards-verification.txt
+
